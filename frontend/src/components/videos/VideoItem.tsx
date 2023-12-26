5aa092b67e74fc34821555e4b023b0f81a1700b1
Constanzamw <cweiner92@gmail.com>
2023-12-26 10:26:00 -0300

    funcional

--- frontend/src/components/videos/VideoItem.tsx
+++ frontend/src/components/videos/VideoItem.tsx
@@ -4,42 +4,44 @@ import "./videoItem.css"
 import { useNavigate } from "react-router-dom"
 import * as videoService from "../../utils/videoService"
 
+
 interface Props{
   video: Video ;
   loadVideos: ()=> void,
 
 }
 
+const VideoItem = ({ video, loadVideos }: Props) => {
+  const navigate = useNavigate();
 
-const VideoItem = ({video , loadVideos}: Props) => {
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm("¿Estás seguro de que quieres eliminar este video?");
+    if (confirmDelete) {
+      try {
+        await videoService.deleteVideo(video._id!);
+        loadVideos();
+      } catch (error) {
+        console.error("Error al eliminar el video:", error);
+      }
+    }
+  };
 
-  const navigate = useNavigate()
-
-  const handleDelete= async(id:string) => {
-    await videoService.deleteVideo(id)
-    loadVideos()
-  }
-  
   return (
-    <div className="col-md-4">   
-     <div className="card card-body video-card" >
-       <div className="d-flex justify-content-between">
-         <h1 onClick={()=> navigate(`/update/${video._id}`) }> {video.title} </h1>
-          <span className="text-danger" onClick={ ()=> video._id && handleDelete(video._id)}>
-              x
+    <div className="col-md-4">
+      <div className="card card-body video-card">
+        <div className="d-flex justify-content-between">
+          <h1 onClick={() => navigate(`/update/${video._id}`)}>{video.title}</h1>
+          <span className="text-danger" onClick={handleDelete}>
+            x
           </span>
         </div>
-          <p> {video.description}</p>
-            <div className="embed-responsive embed-responsive-16by9">
-             <ReactPlayer  
-                url={video.url}
-                width="100%" 
-                height="100%" 
-                className="react-player" />
-            </div>
+        <p>{video.description}</p>
+        <div className="embed-responsive embed-responsive-16by9">
+          <ReactPlayer url={video.url} width="100%" height="100%" className="react-player" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoItem
+export default VideoItem;
