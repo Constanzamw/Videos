7c4a3a58bcd8052a84c6c4e60237222cb17c0266
Constanzamw <cweiner92@gmail.com>
2024-01-24 14:06:18 -0300

    dbbb

--- api/database.ts
+++ api/database.ts
@@ -1,24 +1,23 @@
 import mongoose , { ConnectOptions}from 'mongoose'
 import config from './src/config'
 
-
-(async ()=>{
+(async () => {
   try {
-    const db =  await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,) 
-    console.log ('database is connected to:', db.connection.name)
+      const db = await mongoose.connect(config.MONGO_URI);
+      console.log('Connected to MongoDB:', db.connection.name);
   } catch (error) {
-    console.error(error)
+      console.error(error)
   }
-})()
-// (async () => {
+})();
+
+
+
+// (async ()=>{
 //   try {
-//     await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       // Puedes agregar más opciones según tus necesidades
-//     } as ConnectOptions);
-//     console.log('Database is connected to:', mongoose.connection.name);
+//     const db =  await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,) 
+//     console.log ('database is connected to:', db.connection.name)
 //   } catch (error) {
-//     console.error(error);
+//     console.error(error)
 //   }
-// })();
+// })()
+
--- api/src/config.ts
+++ api/src/config.ts
@@ -2,9 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    MONGO_DATABASE: process.env.MONGO_DATABASE || "videosdb",
-    MONGO_USER: process.env.MONGO_USER || "admin",
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD || "admin",
-    MONGO_HOST: process.env.MONGO_HOST || "0.0.0.0",
+    // MONGO_DATABASE: process.env.MONGO_DATABASE || "videosdb",
+    // MONGO_USER: process.env.MONGO_USER || "admin",
+    // MONGO_PASSWORD: process.env.MONGO_PASSWORD || "admin",
+    // MONGO_HOST: process.env.MONGO_HOST || "0.0.0.0",
+    MONGO_URI: process.env.MONGO_URI || "mongodb+srv://admin:admin@cluster0.i8kplvg.mongodb.net/" ,
     PORT : process.env.PORT || 3000
 }
