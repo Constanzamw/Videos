e5f7e3e3b3d311c5309ef17a8b730ec2db126887
Constanzamw <cweiner92@gmail.com>
2024-01-24 11:49:12 -0300

    fh

--- api/database.ts
+++ api/database.ts
@@ -2,23 +2,23 @@ import mongoose , { ConnectOptions}from 'mongoose'
 import config from './src/config'
 
 
-// (async ()=>{
-//   try {
-//     const db =  await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,) 
-//     console.log ('database is connected to:', db.connection.name)
-//   } catch (error) {
-//     console.error(error)
-//   }
-// })()
-(async () => {
+(async ()=>{
   try {
-    await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // Puedes agregar más opciones según tus necesidades
-    } as ConnectOptions);
-    console.log('Database is connected to:', mongoose.connection.name);
+    const db =  await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,) 
+    console.log ('database is connected to:', db.connection.name)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-})();
+})()
+// (async () => {
+//   try {
+//     await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       // Puedes agregar más opciones según tus necesidades
+//     } as ConnectOptions);
+//     console.log('Database is connected to:', mongoose.connection.name);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
