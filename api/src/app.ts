72c303fc5371a1531959f720009b3015882898b7
Constanzamw <cweiner92@gmail.com>
2024-01-25 10:51:54 -0300

    y

--- api/src/app.ts
+++ api/src/app.ts
@@ -11,19 +11,19 @@ const app = express()
 app.set("PORT", config.PORT);
 
 
-app.use(morgan("dev"));
 app.use(cors({
   origin: 'https://videos-beige-alpha.vercel.app'
 }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+app.use(morgan("dev"));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+//   });
 
 app.use(videosRoutes);
 
