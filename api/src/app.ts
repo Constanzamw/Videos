c94a6a578d845eea3704944e944e111739b28283
Constanzamw <cweiner92@gmail.com>
2024-01-25 10:54:58 -0300

    yu

--- api/src/app.ts
+++ api/src/app.ts
@@ -11,12 +11,10 @@ const app = express()
 app.set("PORT", config.PORT);
 
 
+app.use(morgan("dev"));
 app.use(cors({
   origin: 'https://videos-beige-alpha.vercel.app'
 }));
-app.use(morgan("dev"));
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', "*");
 //     res.header('Access-Control-Allow-Credentials', 'true');
@@ -24,6 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 //     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 //     next();
 //   });
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.use(videosRoutes);
 
